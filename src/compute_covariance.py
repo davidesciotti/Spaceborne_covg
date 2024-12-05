bf5d394d2b220ef7f25c3cb1d448368f8ba8e27f
Sylvain Gouyou Beauchamps <gouyoub@gmail.com>
2024-12-05 12:26:35 +0100

    corrected iNKA

--- src/compute_covariance.py
+++ src/compute_covariance.py
@@ -842,10 +842,16 @@ if part_sky:
     cl_GG_4covsb = cl_GG_unbinned[:, :zbins_use, :zbins_use]
     cl_GL_4covsb = cl_GL_unbinned[:, :zbins_use, :zbins_use]
     cl_LL_4covsb = cl_LL_unbinned[:, :zbins_use, :zbins_use]
+
     if use_INKA:
-        cl_GG_4covnmt = pcl_GG_nmt / fsky
-        cl_GL_4covnmt = pcl_GL_nmt / fsky
-        cl_LL_4covnmt = pcl_LL_nmt / fsky
+        cl_GG_4covnmt = np.zeros_like(cl_GG_unbinned[:, :zbins_use, :zbins_use])
+        cl_GL_4covnmt = np.zeros_like(cl_GL_unbinned[:, :zbins_use, :zbins_use])
+        cl_LL_4covnmt = np.zeros_like(cl_LL_unbinned[:, :zbins_use, :zbins_use])
+        for zi in range(zbins_use):
+            for zj in range(zbins_use):
+                cl_GG_4covnmt[:, zi, zj] = w00.couple_cell(cl_GG_unbinned[:, zi, zj]) / fsky
+                cl_GL_4covnmt[:, zi, zj] = w02.couple_cell(cl_GL_unbinned[:, zi, zj]) / fsky
+                cl_LL_4covnmt[:, zi, zj] = w22.couple_cell(cl_LL_unbinned[:, zi, zj]) / fsky
 
         # TODO not super sure about this
         # cl_GG_4covsb = pcl_GG_nmt[:, :zbins_use, :zbins_use] / fsky
