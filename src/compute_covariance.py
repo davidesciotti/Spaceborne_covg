d12b4087ff446b5fcedc561a7e07742424be1181
Sylvain Gouyou Beauchamps <gouyoub@gmail.com>
2024-12-09 14:28:46 +0100

    fixed iNKA

--- src/compute_covariance.py
+++ src/compute_covariance.py
@@ -576,19 +576,19 @@ cl_GL_4covsb = cl_GL_unbinned[:, :zbins_use, :zbins_use]
 cl_LL_4covsb = cl_LL_unbinned[:, :zbins_use, :zbins_use]
 
 if use_INKA:
-    cl_GG_4covnmt = np.zeros_like(cl_GG_unbinned[:, :zbins_use, :zbins_use])
-    cl_GL_4covnmt = np.zeros_like(cl_GL_unbinned[:, :zbins_use, :zbins_use])
-    cl_LL_4covnmt = np.zeros_like(cl_LL_unbinned[:, :zbins_use, :zbins_use])
+    cl_GG_4covnmt = np.zeros_like(cl_GG_unbinned)
+    cl_GL_4covnmt = np.zeros_like(cl_GL_unbinned)
+    cl_LL_4covnmt = np.zeros_like(cl_LL_unbinned)
     for zi in range(zbins_use):
         for zj in range(zbins_use):
             # setting B-mode related spcectra to 0
             cl_GG_4covnmt[:, zi, zj] = w00.couple_cell([cl_GG_unbinned[:, zi, zj]])[0] / fsky
             cl_GL_4covnmt[:, zi, zj] = w02.couple_cell([cl_GL_unbinned[:, zi, zj],
                                                         np.zeros_like(cl_GL_unbinned[:, zi, zj])])[0] / fsky
-            cl_LL_4covnmt[:, zi, zj] = w22.couple_cell([cl_GL_unbinned[:, zi, zj],
-                                                        np.zeros_like(cl_GL_unbinned[:, zi, zj]),
-                                                        np.zeros_like(cl_GL_unbinned[:, zi, zj]),
-                                                        np.zeros_like(cl_GL_unbinned[:, zi, zj])])[0] / fsky
+            cl_LL_4covnmt[:, zi, zj] = w22.couple_cell([cl_LL_unbinned[:, zi, zj],
+                                                        np.zeros_like(cl_LL_unbinned[:, zi, zj]),
+                                                        np.zeros_like(cl_LL_unbinned[:, zi, zj]),
+                                                        np.zeros_like(cl_LL_unbinned[:, zi, zj])])[0] / fsky
 
 else:
     cl_GG_4covnmt = cl_GG_unbinned
