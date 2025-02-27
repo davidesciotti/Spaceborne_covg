fixed binning of fsky covariance

src/compute_covariance.py
@@ -1108,12 +1108,10 @@ elif part_sky:
     #             noise_3x2pt_5d[probe_A, probe_B, ell_idx, :, :] = noise_3x2pt_4d[probe_A, probe_B, ...]
 
     # TODO return only diag
-    cov_sb_9d = utils.covariance_einsum(cl_3x2pt_5d, noise_3x2pt_5d, fsky,
+    cov_sb_10d = utils.covariance_einsum(cl_3x2pt_5d, noise_3x2pt_5d, fsky,
                                         ells_4covsb, delta_ells_4covsb,
-                                        return_only_diagonal_ells=True)
-    # cov_sb_10d = utils.covariance_einsum(cl_3x2pt_5d, noise_3x2pt_5d, fsky,
-    # ells_4covsb, delta_ells_4covsb,
-    # return_only_diagonal_ells=False)
+                                        return_only_diagonal_ells=False)
+
     bin_cov_sb_10d = np.zeros((n_probes, n_probes, n_probes, n_probes, nbl_eff,
                                nbl_eff, zbins_use, zbins_use, zbins_use, zbins_use))
 
@@ -1167,19 +1165,14 @@ elif part_sky:
                 probename_dict[block_name[0]], probename_dict[block_name[1]], \
                 probename_dict[block_name[2]], probename_dict[block_name[3]]
 
-            if cov_sb_9d[probe_idxs][:, zi, zj, zk, zl].shape != (nbl_eff):
+            if cov_sb_10d[probe_idxs][:, :, zi, zj, zk, zl].shape != (nbl_eff, nbl_eff):
                 print(f'Binning Spaceborne {block_name} covariance')
 
-                binned_block_1d = utils.bin_cell(cls_in=cov_sb_9d[probe_idxs][:, zi, zj, zk, zl],
-                                                 ells_in=ells_4covsb, ells_out=ells_eff,
-                                                 ells_out_edges=ells_eff_edges, weights=None,
-                                                 which_binning='mean', ells_eff=ells_eff)
-                # I get the same result with
-                # binned_block_1d = bin_obj.bin_cell(cov_sb_9d[probe_idxs][:, zi, zj, zk, zl])
-
-                # fill the diagonal
-                bin_cov_sb_10d[probe_idxs][:, :, zi, zj, zk, zl] = np.diag(binned_block_1d)
-
+                bin_cov_sb_10d[probe_idxs][:, :, zi, zj, zk, zl] = \
+                    utils.bin_2d_matrix(cov=cov_sb_10d[probe_idxs][:, :, zi, zj, zk, zl],
+                                        ells_in=ells_tot, ells_out=ells_eff,
+                                        ells_out_edges=ells_eff_edges, weights=None,
+                                        which_binning='mean')
                 # TODO delete this
                 # bin_cov_sb_10d[probe_idxs][:, :, zi, zj, zk, zl] = \
                 #     utils.bin_cell(cls_in=cov_sb_9d[probe_idxs][:, zi, zj, zk, zl],